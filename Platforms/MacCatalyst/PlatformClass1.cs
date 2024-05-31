bf1a4bf8fd9f84fbe2a0469dcbd3d46bbd37f737
Janus <Janus@DESKTOP-2D4N35I>
2024-05-30 21:05:20 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsumoApi2
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsumoApi2
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
